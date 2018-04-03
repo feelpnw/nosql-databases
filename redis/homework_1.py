6b4b616ad5a02f02b9676cb565832066b9080f0c
Ian <feelpnw@gmail.com>
2018-04-03 16:33:39 -0400

    file modified

--- redis/homework_1.py
+++ redis/homework_1.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python 
 
-# Require the httparty gem
+# Jeeho Song 
+# js4892
+# get request 
 
+# Require the httparty gem (in Ruby)
 # Set up the url and send a GET request to it. The base url is:
 # "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
-
+# => "https://api.nasa.gov/planetary/apod?api_key=RVhEoSzXOlHF1ppmniBuNqFaBp9qI6D7sGlQZINQ"
 # Make the request and print out the "url" key in the response, which is the image url
 
-# "https://api.nasa.gov/planetary/apod?api_key=RVhEoSzXOlHF1ppmniBuNqFaBp9qI6D7sGlQZINQ"
 
 import requests 
 import json 
@@ -15,17 +17,10 @@ import json
 url = 'https://api.nasa.gov/planetary/apod?api_key=RVhEoSzXOlHF1ppmniBuNqFaBp9qI6D7sGlQZINQ'
 
 param = {"date": "2017-10-02"}
-r = requests.get(url, params=param)
+r = requests.get(url=url, params=param)
 data = r.json()
 image_url = data['url']
 
 print (image_url)
 
 
-
-
-
-# r = requests.get(url) #(url=url, params=params)
-# data = r.json()
-
-# data['date'] = '2017-10-02'
