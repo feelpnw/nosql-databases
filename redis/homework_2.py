ba00782bbf9ca87618be8e70eb09c5ef981039a7
Song <song@Songs-MacBook-Air.local>
2018-04-10 23:57:31 -0400

    updated

--- redis/homework_2.py
+++ redis/homework_2.py
@@ -8,8 +8,8 @@ VOTE_SCORE = 432
 def article_vote(redis, user, article):
 	#2018-04-11 01:23:59.118211 - 7 days, 0:00:00
     cutoff = datetime.datetime.now() - datetime.timedelta(seconds=ONE_WEEK_IN_SECONDS)
-	
-    print(cutoff)
+    # print(cutoff)
+    # 
     if not datetime.datetime.fromtimestamp(redis.zscore('time:', article)) < cutoff:
         article_id = article.split(':')[-1]
         
@@ -26,27 +26,43 @@ def article_switch_vote(redis, user, from_article, to_article):
 
     redis.srem('voted:' + from_article_id, user)
     redis.sadd('voted:' + to_article_id, user)
+    # or redis.smove('voted:' + from_article_id, 'voted:' + to_article_id, user)
     
+
+    from_article_score = redis.zscore('score:', 'article:'+ from_article_id)
+    changed_from_article_score = from_article_score - 432
+    to_article_score = redis.zscore('score:', 'article:'+ to_article_id)
+    changed_to_article_score = to_article_score + 432
+
+    redis.zrem('score:', 'article:'+ from_article_id)
+    redis.zadd('score:', changed_from_article_score, 'article:'+ from_article_id)
+    redis.zrem('score:', 'article:'+ to_article_id)
+    redis.zadd('score:', changed_to_article_score, 'article:'+ to_article_id)
+
+    
+	# redis.zincrby('score:', -432, 'article:'+ from_article_id)
 
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # user:3 up votes article:1
 article_vote(redis, "user:3", "article:1")
-# print(article_vote(redis, "user:3", "article:1"))
 
 # user:3 up votes article:3
 article_vote(redis, "user:3", "article:3")
-print(article_vote(redis, "user:3", "article:3"))
 
 # user:2 switches their vote from article:8 to article:1
 article_switch_vote(redis, "user:2", "article:8", "article:1")
-a = "article:8"
-b = a.split(':')[-1]
-print(b)
+
 
 # Which article's score is between 10 and 20?
 # PRINT THE ARTICLE'S LINK TO STDOUT:
 # HOMEWORK 2 Part II
 article = redis.zrangebyscore("score:", 10, 20) 
-print(redis.hget(article, "link"))
+print(article)
+print(redis.hget("article:8", "link"))
+
+
+
+
+
 
